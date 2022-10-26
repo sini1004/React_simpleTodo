601765f58b8778461b794b7fcdace2c0a4e88cdf
shinhee <tlsgml656@gmail.com>
2022-10-26 12:57:35 +0900

    item,board 컴포넌트 생성 및 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import './App.css';
+import TodoBoard from './components/TodoBoard';
 
 function App() {
   const [inputValue, setInputValue] = useState();
+  const [todoList, setTodoList] = useState([]); //array를 이용해서 아이템을 모아줌
+
+  const addItem = () => {
+    console.log('아이템 함수 발생', inputValue);
+    setTodoList([...todoList, inputValue]);//기존의 아이템을 유지하고 새로운 아이템 추가
+  }
+
   return (
     <>
-      <input type="text" onChange={(event => console.log(event.target.value))} value={inputValue}/>
-      <button>추가</button>
+      <input type="text" onChange={(event) => setInputValue(event.target.value)} value={inputValue || ""}/>
+      <button onClick={addItem}>추가</button>
+      <TodoBoard />
     </>
   );
 }
---
+++ src/components/TodoBoard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TodoItem from './TodoItem';
+
+function TodoBoard() {
+  return (
+    <>
+      <h1>TodoBoard</h1>
+      <TodoItem />
+      <TodoItem />
+    </>
+  )
+}
+
+export default TodoBoard
